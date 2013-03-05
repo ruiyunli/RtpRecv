8dc3f6ea9f29cbee93132a00661ed48d9e7269cd
ruiyunli <ruiyunli@126.com>
2013-03-05 18:25:37 +0800

    SharpToRecv

--- jni/rtp-jni.cpp
+++ jni/rtp-jni.cpp
@@ -1,21 +1,31 @@
 
 /*
- * RTP传输流程：
- * 1、配置信息
- * 	     包括本地端口，目标端口，目标ip，发送的数据包次数
- * 2、ip数据结构转换
- * 3、时间戳设置
- * 4、创建会话
- * 5、发送及接受数据包
+ *****************************************
+ * RTP传输流程：                                                                    *
+ * 1、配置信息							 *
+ * 	     包括本地端口，目标端口，目标ip，             *
+ * 	     以及发送的数据包次数				 *
+ * 2、ip数据结构转换						 *
+ * 3、时间戳设置							 *
+ * 4、创建会话							 *
+ * 5、发送及接受数据包					 *
+ *****************************************
+ *
+ ******************************************
+ * Send:  	192.168.0.30--->192.168.0.20  *
+ * 			10000--->12000				  *
+ * Recv:	192.168.0.20--->192.168.0.30  *
+ * 			12000--->10000				  *
+ ******************************************
  *
  */
 
 #include <jni.h>
-#include "rtpsession.h"
-#include "rtpudpv4transmitter.h"
-#include "rtpipv4address.h"
-#include "rtpsessionparams.h"
-#include "rtperrors.h"
+#include "rtpsession.h"				//定义了rtpsession的一些实现
+#include "rtpudpv4transmitter.h"	//定义了RTPSession的第二个参数类
+#include "rtpipv4address.h"			//定义了rtpipv4address
+#include "rtpsessionparams.h"		//定义了rtpsession的第一个参数类
+#include "rtperrors.h"				//定义了RTP中的错误信息
 #include <netinet/in.h>	//not win32
 #include <arpa/inet.h>	//not win32
 #include <stdlib.h>
@@ -50,60 +60,45 @@ void Java_com_example_rtprecv_MainActivity_RtpTest(JNIEnv *, jobject);
 
 void Java_com_example_rtprecv_MainActivity_RtpTest(JNIEnv* env, jobject thiz)
 {
- 	RTPSession sess;
-	uint16_t portbase,destport;
-	uint32_t destip;
-	std::string ipstr;
+ 	RTPSession sess;	//RTPSession类来实例化此次的RTP会话
+	uint16_t portbase;
 	int status,i,num;
 
     //1、配置信息
-	//包括本地端口，目标端口，目标ip，发送的数据包次数
+	//包括本地端口
 	portbase=LOCAL_PORT;
-    ipstr=DEST_IP;
-    destport=DEST_PORT;
 	num=PACKET_NUM;
-
-	//2、ip数据结构转换
-	destip = inet_addr(ipstr.c_str());
-	if (destip == INADDR_NONE)
-	{
-		LOGE("BAD IP ADDR");
-		return ;
-	}
-	//BYTE ORDER到host byte order转换
-	destip = ntohl(destip);
 	
+	//下面创建一个RTP会话，发送传入的数据包
+
+	//这是rtpsession的第二个参数类，他的成员函数可以设定监听端口
 	RTPUDPv4TransmissionParams transparams;
+
+	//这是rtpsession的第一个参数类，他的成员函数可以设置恰当的时戳单元
 	RTPSessionParams sessparams;
 	
 	//3、时间戳设置
+	//设置恰当的时戳单元，每秒我们需要发送10次，故参数为1.0/10
 	sessparams.SetOwnTimestampUnit(TIME_STAMP);
+	//下面设置是不是接收我们自定义的数据包，这里选是
 	sessparams.SetAcceptOwnPackets(true);
+	//设置本机端口
 	transparams.SetPortbase(portbase);
 
 	//4、创建会话
-	status = sess.Create(sessparams,&transparams);	
-//	checkerror(status);
-	RTPIPv4Address addr(destip,destport);
-	status = sess.AddDestination(addr);
-//	checkerror(status);
+	status = sess.Create(sessparams,&transparams);
 
 	LOGD("LocalPort:%d",portbase);
-	LOGD("DestPort:%d",destport);
-	LOGD(DEST_IP);
-	LOGD("ready!");
+	LOGD("ready");
 
 	//5、发送及接受数据包
 	for (i = 1 ; i <= num ; i++)
 	{
-		// send the packet
-		status = sess.SendPacket((void *)"1234567890",10,0,false,10);
-
-		LOGD("send packet:%d",i);
-
+		//为了保证source table（数据源列表）不会改动，类似GotoFirstSourceWithData等函数
+		//的执行必须要在BeginDataAccess和EndDataAccess之间，详情见库函数声明
 		sess.BeginDataAccess();
 
-		// check incoming packets
+		//收到的报文，遍历所有携带数据的源（因为一个rtp会话允许有多个参与者（源））
 		if (sess.GotoFirstSourceWithData())
 		{
 			do
@@ -135,4 +130,5 @@ void Java_com_example_rtprecv_MainActivity_RtpTest(JNIEnv* env, jobject thiz)
 
 	return;
 }
+
 
